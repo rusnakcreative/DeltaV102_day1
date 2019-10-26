d50ec365c2eecd1dabf5f4de2834ad3f94e3c0e6
Mike Rusnak <rusnakcreative@gmail.com>
2019-10-26 10:46:18 -0500

    Setup p5 canvas

---
+++ app.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(640, 480);
+}
+
+function draw() {
+
+    stroke(0,0,0);
+    strokeWeight(3);
+    line(0,40,640,40);
+
+    noStroke();
+    fill(0,255,0);
+    rect(310, 230, 20, 20);
+
+    fill(255,0,0)
+    circle(500,100,20)
+
+
+}
