40d57e07b158e6c6d940c4f1f3adb29c04238744
Mike Rusnak <rusnakcreative@gmail.com>
2019-10-26 11:57:54 -0500

    Concatenation is fungit add .git add .!

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ function day1AM() {
     var firstName = "Mike";
     var age = 34;
     var heightInches = 75;
-    
+    var feet = Math.floor(heightInches/12);
+    var inches = heightInches % 12;
+    age = age + 1;
+
+    console.log(firstName + " is " + age + " years old.")
+    console.log(firstName + " is " + feet + " foot " + inches + " inches tall.")
 }
 
 // To call the functoin: 
